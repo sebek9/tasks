409c56c8f5ab395839b02e6040a8db6eda604982
sebastiankalinkowski <Polska500+>
2020-02-26 18:39:54 +0100

    add cross origin

--- src/main/java/com/crud/tasks/controller/TrelloController.java
+++ src/main/java/com/crud/tasks/controller/TrelloController.java
@@ -1,19 +1,15 @@
 package com.crud.tasks.controller;
 
-import com.crud.tasks.domain.CreatedTrelloCard;
-import com.crud.tasks.domain.TrelloBoardDto;
-import com.crud.tasks.domain.TrelloCardDto;
-import com.crud.tasks.mapper.TaskMapper;
-import com.crud.tasks.service.TrelloService;
 
+import com.crud.tasks.domain.TrelloBoardDto;
 import com.crud.tasks.trello.client.TrelloClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-import java.util.Optional;
 import java.util.List;
 
 @RestController
 @RequestMapping("/v1/trello")
+@CrossOrigin(origins = "*")
 public class TrelloController {
 
     @Autowired
